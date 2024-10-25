03f4128e4751a26a69be1a6ce7330adb3212c38f
carlosandresamp <149840865+carlosandresamp@users.noreply.github.com>
2024-10-25 12:56:26 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# SpeekME_IA
+ O SpeekME IA é um programa que utiliza a API Generative AI do Google para gerar respostas de voz a partir de entradas de fala do usuário. Ele também inclui funcionalidades para reconhecimento de fala e síntese de voz.
---
+++ SpeekME IA.py
@@ -0,0 +1,116 @@
+# Importa a biblioteca do Google para inteligência artificial generativa
+# que possibilita a criação de respostas e interações automáticas.
+import google.generativeai as genai
+
+# Importa a biblioteca de reconhecimento de fala, usada para converter
+# áudio em texto, permitindo interações por comando de voz.
+import speech_recognition as sr
+
+# Importa a biblioteca de síntese de fala, que transforma texto em áudio.
+# Isso permite que a aplicação responda em voz, tornando a interação mais completa.
+import pyttsx3
+
+
+# Sua chave da API Generative AI do Google
+google_api_key = 'AIzaSyBa2nysC8S8Xi8ldicfLx0zpKZ_3wZLeBM'
+# Configura a API Generative AI com sua chave
+genai.configure(api_key=google_api_key)
+
+generation_config = {
+    'candidate_count': 1,
+    'temperature': 0.5,
+}
+
+# Configurações para gerar texto com o modelo Gemini 1.0 Pro
+
+model = genai.GenerativeModel(model_name='gemini-1.0-pro',
+                              generation_config=generation_config
+                              )
+
+# Carrega o modelo Gemini 1.0 Pro com as configurações definidas
+chat = model.start_chat(history=[])
+
+# Inicializa o mecanismo de sistese de voz.
+motor_de_sistese_de_fala = pyttsx3.init()
+
+# Obtém todas as vozes disponíveis no sistema
+voices = motor_de_sistese_de_fala.getProperty('voices')
+
+# Define a voz a ser usada pelo robô como a segunda voz
+motor_de_sistese_de_fala.setProperty('voice', voices[0].id)
+
+# Usa o motor de síntese de fala para falar
+def falar(frase):
+    motor_de_sistese_de_fala.say(frase)
+    motor_de_sistese_de_fala.runAndWait()
+    motor_de_sistese_de_fala.stop()
+
+
+# Remove os caracteres especiais do texto gerado
+def remover_caracteres_especiais(texto):
+    texto_sem_asteriscos = ""
+    for caractere in texto:
+        if caractere not in "*!@$%&''-+.*/=+-_":
+            texto_sem_asteriscos += caractere
+    return texto_sem_asteriscos
+
+
+# Define uma função para reconhecer a fala do usuário.
+def reconhecer_fala():
+    # Inicializa o reconhecedor de fala.
+    microfone = sr.Recognizer()
+
+    # Configura o microfone como fonte de áudio.
+    with sr.Microphone() as source:
+        # Ajusta o reconhecimento para o ruído ambiente.
+        microfone.adjust_for_ambient_noise(source)
+
+        # Fala uma mensagem para o usuário.
+        falar('Sobre o que você quer conversar? ')
+
+        # Escuta o áudio do microfone.
+        audio = microfone.listen(source)
+
+        # Tenta reconhecer a fala 5 vezes.
+
+        for _ in range(5):
+            try:
+                # Reconhece a fala do usuário usando o Google Speech Recognition.
+                frase_reconhecida = microfone.recognize_google(audio, language='pt')
+
+                # Fala a frase reconhecida ao usuário.
+                falar('Você disse: '), falar(frase_reconhecida)
+
+                # Retorna a frase reconhecida.
+
+                return frase_reconhecida
+
+            except sr.UnknownValueError:
+                falar('Não entendi o que você disse, vou encerrar a conversa.')
+                break
+            except sr.RequestError as e:
+                falar(f'Ocorreu um erro ao reconhecer a fala: {e}')
+                break
+
+    return None
+
+
+# Saudação inicial
+falar('Seja bem vindo chefe, como posso lhe servir')
+falar('Para encerrar a conversa diga FIM')
+
+
+# Loop principal da conversa
+def iniciar_conversa():
+    prompt = reconhecer_fala()
+    while prompt != 'fim':
+        # Envia a fala do usuário para o modelo gerar uma resposta
+        response = chat.send_message(prompt)
+        # Remove caracteres especiais da resposta do modelo (opcional)
+        falar(remover_caracteres_especiais(response.text))
+        # Obtém a próxima fala do usuário
+        prompt = reconhecer_fala()
+    falar('Você encerrou a conversa')
+
+
+iniciar_conversa()
